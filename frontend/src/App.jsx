04ba9e4c6d0f7588bd9dff444ef7e377fd6e1169
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-09-18 11:27:43 +0530

    Add the Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import SignInPage from "./pages/SignInPage"
 import SignUpPage from "./pages/SignUpPage"
 import Dashborad from "./pages/Dashborad"
 import Header from "./components/Header"
+import Footer from "./components/FooterComponent"
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route path='/sign-up' element={<SignUpPage />} />
         <Route path='/dashborad' element={<Dashborad />} />
       </Routes>
+      <Footer />
     </Router>
   )
 }
