41697ed721cf036d49f702022945a1573b66a2b2
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-09-30 09:27:07 +0530

    Show user posts inside dashboard

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,10 @@
-import flowbite from "flowbite-react/tailwind"
+import flowbite from "flowbite-react/tailwind";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {},
   },
-  plugins: [
-    flowbite.plugin(),
-  ],
-}
-
+  plugins: [require("flowbite/plugin"), require("tailwind-scrollbar")],
+};
