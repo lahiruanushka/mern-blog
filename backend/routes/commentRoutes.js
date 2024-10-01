819bb4a83f5eac227676a17ca2408184dfcd7a6d
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-10-01 10:31:01 +0530

    Create API routes for fetching post comments and user details

--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createComment } from '../controllers/commentController.js';
+import { createComment, getPostComments } from '../controllers/commentController.js';
+
 const router = express.Router();
+
 router.post('/create', verifyToken, createComment);
+router.get('/getPostComments/:postId', getPostComments);
+
 export default router;
