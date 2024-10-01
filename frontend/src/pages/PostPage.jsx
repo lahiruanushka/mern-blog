e1c12a77fc41b066ae5621f5c1e4e2604cd3db83
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-10-01 10:08:06 +0530

    Add comment section to post page

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
-import { Button, Spinner } from 'flowbite-react';
-import { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Button, Spinner } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import CommentSection from "../components/CommentSection";
 
 const PostPage = () => {
   const { postSlug } = useParams();
@@ -67,6 +68,8 @@ const PostPage = () => {
         className="p-3 max-w-2xl mx-auto w-full post-content"
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
+      {/* Comment Section */}
+      <CommentSection postId={post._id} />
     </main>
   );
 };
