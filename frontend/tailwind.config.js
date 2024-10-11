1fc39997704140ae179fe7f311eb2be03dd7fa0c
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-10-11 10:51:36 +0530

    add recent article section to the post page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,5 +6,9 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin"), require("tailwind-scrollbar")],
+  plugins: [
+    require("flowbite/plugin"),
+    require("tailwind-scrollbar"),
+    require("@tailwindcss/line-clamp"),
+  ],
 };
